bb0bc43e70fc131af41148900c57d1f2b2aac59f
Shrinath Gupta <shrinath.gupta@hp.com>
2019-08-02 03:00:18 +0530

    Quick fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ const appRoutes: Routes = [
 			}
 		]
 	},
+	{ path: 'account', component: HomeComponent, canLoad: [] },
 	{ path: '', component: HomeComponent },
 	{ path: '**', component: PageNotFoundComponent }
 ];
